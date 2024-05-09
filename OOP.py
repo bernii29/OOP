c5984b131aa12dc9f76b189ff25a5e5c045e7c1b
Berni <juhaszberni29@gmail.com>
2024-05-09 19:00:20 +0200

    foglalas

--- OOP.py
+++ OOP.py
@@ -10,3 +10,30 @@ class EgyagyasSzoba:
 class KetagyasSzoba:
     def __init__(self, szobaszam: int, ar: int):
         super().__init__(szobaszam, ar)
+
+class Szalloda:
+    def __init__(self, nev):
+        self.szobak=[]
+
+    def szoba_hozzaad(self, szoba):
+        self.szobak.append(szoba)
+
+class Foglalas:
+    def __init__(self, szoba, datum, nev):
+        self.szoba=szoba
+        self.datum=datum
+        self.nev=nev
+
+class FoglalasKezeles:
+    def __init__(self, szalloda):
+        self.szalloda=szalloda
+        self.foglalasok=[]
+
+    def foglalas(self, szobaszam, datum, nev):
+        for szoba in self.szalloda.szobak:
+            if szoba.szobaszam==szobaszam:
+                self.foglalasok.append(Foglalas(szoba, datum, nev))
+                return szoba.ar
+        return None
+
+    def lemondas(self, szobaszam, datum, nev):
