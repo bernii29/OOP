cab7ef4501877f41a846e1f88284add5059dabf3
Berni <juhaszberni29@gmail.com>
2024-05-09 19:33:48 +0200

    lemondás és lisázás

--- OOP.py
+++ OOP.py
@@ -19,21 +19,29 @@ class Szalloda:
         self.szobak.append(szoba)
 
 class Foglalas:
-    def __init__(self, szoba, datum, nev):
+    def __init__(self, szoba, datum):
         self.szoba=szoba
         self.datum=datum
-        self.nev=nev
 
 class FoglalasKezeles:
     def __init__(self, szalloda):
         self.szalloda=szalloda
         self.foglalasok=[]
 
-    def foglalas(self, szobaszam, datum, nev):
+    def foglalas(self, szobaszam, datum):
         for szoba in self.szalloda.szobak:
             if szoba.szobaszam==szobaszam:
-                self.foglalasok.append(Foglalas(szoba, datum, nev))
+                self.foglalasok.append(Foglalas(szoba, datum))
                 return szoba.ar
         return None
 
-    def lemondas(self, szobaszam, datum, nev):
+    def lemondas(self, szobaszam, datum):
+        for foglalas in self.foglalasok:
+            if foglalas.szoba.szobaszam==szobaszam and foglalas.datum==datum:
+                self.foglalasok.remove(foglalas)
+                return True
+        return False
+
+    def listazas(self):
+        for foglalas in self.foglalasok:
+            print(f"Szoba: {foglalas.szoba.szobaszam}, Dátum: {foglalas.datum}")
